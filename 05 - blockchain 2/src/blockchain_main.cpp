9a79cec8119e01f8650d7d5fd7ea2f430c765d2e
JonasVsc <jonasvasconcelosbarbosa@gmail.com>
2023-11-25 12:21:13 -0300

    Alocação e seleção de Threads

--- 05 - blockchain 2/src/blockchain_main.cpp
+++ 05 - blockchain 2/src/blockchain_main.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<thread>
-
+#include <windows.h>
 #include"blockchain.h"
 #include"wallet.h"
 
@@ -8,80 +8,60 @@
 int main()
 {
     Blockchain blockchain;
-    Crypto amount;
-    Sender sender;
-    Receiver receiver;
+    int index;
     char opt;
+    std::thread* selectedThread;
+
+    SYSTEM_INFO systemInfo;
+    GetSystemInfo(&systemInfo);
+
+    std::cout << "Numero maximo de threads suportado pelo sistema: " << systemInfo.dwNumberOfProcessors << '\n';
+    system("pause");
+
+    std::thread* Thread = new std::thread [systemInfo.dwNumberOfProcessors - 1];
 
     
     while(true)
     {
         system("cls");
 
-        std::cout << "Thread [] Saldo: " << '\n' << '\n';
+        std::cout << "Thread [" << index <<  "]" << '\n' << '\n'
+        << "Saldo 00 Crypto" << '\n' << '\n';
 
         std::cout
         << "[0] - Finalizar" << '\n'
-        << "[1] - Realizar Transacao" << '\n'
-        << "[2] - Historico de Transacoes da Conta" << '\n'
-        << "[3] - Buscar Transacao" << '\n'
-        << "[4] - Minerar" << '\n'
-        << "[5] - Criar Carteira" << '\n'
-        << "[6] - Criar Thread" << '\n'
-        << "[7] - Selecionar Thread" << '\n'
-        << "[8] - Listar Transacoes de um bloco" << '\n';
-        
-
-
-
-
-
+        << "[1] - Selecionar Thread" << '\n'
+        << "[2] - Criar Carteira" << '\n';
         std::cin >> opt;
 
-        
-        if(opt == '0')
-        {
-            exit(0);
-        }
 
         if(opt == '1')
         {
-            system("cls");
-
-            Transaction* transaction = new Transaction;
-
-            std::cout << "Transacao\n\n";
-            std::cout << "Quantia: ";
-            std::cin >> amount;
-
-            std::cout << "De: ";
-            std::cin >> sender;
-
-            std::cout << "Para: ";
-            std::cin >> receiver;
-            
-            system("cls");
-
-            std::cout 
-            << "Enviando" << amount << '\n'
-            << "De: " << sender << '\n'
-            << "Para: " << receiver;
-
+            std::cout << "Thread: ";
+            std::cin >> index;
+            if(index < systemInfo.dwNumberOfProcessors)
+            {
+                selectedThread = &Thread[index];
+            } else {
+                std::cerr << "Erro, Thread Invalida" << '\n';
+                index = 0;
+                system("pause");
+            }
         }
 
-        if(opt == '5')
+
+
+
+
+
+
+
+
+
+
+        if(opt == '0')
         {
-            system("cls");
-            Wallet* wallet = new Wallet;
-            
-            std::cout
-            << "Nova carteira criada" << '\n' << '\n'
-            << "Chave Privada: \n" << wallet->privateKey << '\n' << '\n'
-            << "Chave Publica: \n" << wallet->publicKey << '\n' << '\n';
-
-            std::cout << "\nAnote suas chaves!\n\n";
-
-            system("pause");
+            exit(0);
         }
     }
 
